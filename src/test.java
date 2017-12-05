2cde70f62569c8e1384d81eebd04fd5565d41a74
lolgame99 <lolgame99@gmail.com>
2017-12-05 12:05:18 +0100

    Init

---
+++ src/test.java
@@ -0,0 +1,7 @@
+import org.lwjgl.Version;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("LWJGL Version " + Version.getVersion() + " is working.");
+    }
+}
